69adc9c223581452c80bd135626bddc85c2c5a5a
jmyager <jmyager6@gmail.com>
2018-05-21 12:39:51 -0400

    finished logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 var friends = [
     {
-        "name": "Josh",
-        "photo": "https://google.com/search?josh",
-        "scores":[1,2,5,4,6,7,9,8]
+        "name": "Dog",
+        "photo": "https://google.com/search?Dog",
+        "scores":[1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        "name": "Cat",
+        "photo": "https://google.com/search?Cat",
+        "scores": [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        "name": "Otter",
+        "photo": "https://google.com/search?Otter",
+        "scores": [5,4,3,2,1,1,2,3,4,5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,37 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
+var userSurvey = [];
+var minScore = 500;
+var totalScore = 0;
+var match;
 
-module.exports = function(app) {
 
-    app.get("/api/friends", function(req, res) {
-        res.JSON(friendData);
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.JSON(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
-        console.log("Received incoming survey results");
+    app.post("/api/friends", function (req, res) {
+        userSurvey = req.body.scores;
+        console.log("userSurvey: " + userSurvey);
+        console.log("friends: " + friends);
+        console.log("friends[0].name: " + friends[0].name);
+        console.log("friends[0].scores: " + friends[0].scores);
+        for (var i = 0; i < friends.length; i++) {
+            totalScore = 0;
+            console.log("Checking... " + friends[i].name)
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var score = Math.abs(userSurvey[i] - friends[i].scores[j]);
+                totalScore += score;
+            }
+            if (totalScore < minScore) {
+                minScore = totalScore;
+                match = friends[i].name;
+                console.log("minScore was changed with " + friends[i].name);
+            }
+            console.log(friends[i].name + "'s total score is: " + totalScore);
+        }
+        console.log("Your match is " + match + "!");
     });
 
 };
