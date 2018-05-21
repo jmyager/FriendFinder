4ec60d78d77683b2b7286342273ecba44b5d71c5
jmyager <jmyager6@gmail.com>
2018-05-21 12:41:25 -0400

    added alert

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,13 +13,8 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         userSurvey = req.body.scores;
-        console.log("userSurvey: " + userSurvey);
-        console.log("friends: " + friends);
-        console.log("friends[0].name: " + friends[0].name);
-        console.log("friends[0].scores: " + friends[0].scores);
         for (var i = 0; i < friends.length; i++) {
             totalScore = 0;
-            console.log("Checking... " + friends[i].name)
             for (var j = 0; j < friends[i].scores.length; j++) {
                 var score = Math.abs(userSurvey[i] - friends[i].scores[j]);
                 totalScore += score;
@@ -27,11 +22,9 @@ module.exports = function (app) {
             if (totalScore < minScore) {
                 minScore = totalScore;
                 match = friends[i].name;
-                console.log("minScore was changed with " + friends[i].name);
             }
-            console.log(friends[i].name + "'s total score is: " + totalScore);
         }
-        console.log("Your match is " + match + "!");
+        alert("Your match is " + match + "!");
     });
 
 };
